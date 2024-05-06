a472d2742404f708afa66a6913339febac8941b5
jaimemaureira01 <jaime_maureira_a@hotmail.es>
2024-05-05 23:23:32 -0400

    first commit

---
+++ validate.js
@@ -0,0 +1,16 @@
+
+var nombre = document.getElementById('nombre');
+var password = document.getElementById('password');
+var error = document.getElementById('error')
+
+function validateLogin(){
+
+    console.log('Enviando formulario...');
+
+    var mensajeError =[];
+
+    if (nombre.value == null || nombre.value == '' || nombre.value ){
+
+        
+    }
+}
